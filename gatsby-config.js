efcdca2928b33712055302d0f31998766c48cf3a
Ryan Doyle <doylecodes@gmail.com>
2019-06-24 20:12:27 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,23 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "Pandas Eating Lots",
+  }, 
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
